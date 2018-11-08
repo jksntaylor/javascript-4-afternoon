ed803951c8d2582a84c72832d99746128b49eade
Jackson Taylor <josephjacksontaylor@gmail.com>
2018-11-08 16:29:46 -0700

    green screen

--- closures.js
+++ closures.js
@@ -23,12 +23,14 @@ function outer() {
 */
   
 // Code Here
+var inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+inner();
 
 
 
@@ -52,6 +54,9 @@ function callFriend(name) {
 */
 
 //Code Here
+var callJake = new callFriend('Jake');
+
+callJake('435-555-9248');
 
 
 
@@ -62,15 +67,22 @@ function callFriend(name) {
 */
 
 //Code Here
+function makeCounter () {
+  var num = 0
+  return function increment() {
+    num++;
+    return num;
+  }
+}
 
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+// Uncomment this once you make your function
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,9 +99,17 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  var num = value;
   return {
+    inc: function() {
+      num++;
+      return num;
+    }, 
 
+    dec() {
+      num--;
+      return num;
+    }
   };
 }
 
@@ -113,9 +133,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message () {
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +167,9 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function() {
+      return privateMethod(); 
+    }
   };
 })();
 
@@ -163,6 +189,16 @@ function secretNumber() {
 
   return {
     // Code here
+      addToSecret (num) {
+        secret += num;
+        return secret;
+      },
+
+      takeAwayFromSecret(num) {
+        secret -= num;
+        return secret;
+      }
+
   };
 }
 
@@ -187,10 +223,15 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
+  var j = 0;
   for (var i = 0; i <= 5; i++) {
     setTimeout(function() {
-      console.log(i);
+      console.log(j);
+      inc();  
     }, i * 1000);
+  }
+  function inc() {
+    j++;
   }
 }
 timeOutCounter();
